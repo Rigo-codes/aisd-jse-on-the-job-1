85322e044a741419ad3fc052ca8679caa706a23a
Rigo-codes <rodrigo.s.m.jr@gmail.com>
2024-11-04 22:50:01 -0800

    added store nams and type to JS file. Also added product and price variables with values.

---
+++ WagDayCare/index.js
@@ -0,0 +1,16 @@
+const storeName = "Easy Drip Coffee";
+const storeType = "Coffee shop"
+
+const product1 = "Espresso"
+const product2 = "Latte"
+const product3 = "Cappuccino"
+const product4 = "Cortado"
+const product5 = "Cold Brew"
+const product6 = "Add Syrup"
+
+const price1 = 3.00;
+const price2 = 5.50;
+const price3 = 4.50;
+const price4 = 3.50;
+const price5 = 5.00;
+const price6 = .75;
