b327d892388487a4f53aa96d8ca010588aa11f2e
Rigo-codes <rodrigo.s.m.jr@gmail.com>
2024-11-04 23:00:41 -0800

    added a variable of store hours and console logs for testing.

--- WagDayCare/index.js
+++ WagDayCare/index.js
@@ -14,3 +14,14 @@ const price3 = 4.50;
 const price4 = 3.50;
 const price5 = 5.00;
 const price6 = .75;
+
+const storeHours = "Store hours: Monday - Friday: 10am - 6pm, Saturdau - Sunday: 10am - 4pm"
+
+console.log(storeName, storeType)
+console.log(product1, price1)
+console.log(product2, price2)
+console.log(product3, price3)
+console.log(product4, price4)
+console.log(product5, price5)
+console.log(product6, price6)
+console.log(storeHours)
